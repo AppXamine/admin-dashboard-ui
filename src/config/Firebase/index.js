3e2ab539d8219558b4cf9548f3e1eed300d67530
Anas Ahmed <muhammadanas61222@gmail.com>
2023-11-14 17:27:31 +0500

    initialization

--- src/config/Firebase/index.js
+++ src/config/Firebase/index.js
@@ -5,13 +5,7 @@ import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword} from "fire
 import { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZQraVa6GQ08eq553-BlgYWj5pj0CRuZ4",
-  authDomain: "officeproject-e1d30.firebaseapp.com",
-  projectId: "officeproject-e1d30",
-  storageBucket: "officeproject-e1d30.appspot.com",
-  messagingSenderId: "671693441805",
-  appId: "1:671693441805:web:169d828e78db9cc6577e44",
-  measurementId: "G-9V81DDFFZS"
+
 };
 
 
